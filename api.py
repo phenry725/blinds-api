903f60da8f76aa13c8c2625beb347688b5c87251
Paul Henry <phenry@mulesoft.com>
2017-09-22 13:29:18 -0700

    update w/ things I learned from the doc

--- api.py
+++ api.py
@@ -8,30 +8,34 @@ try:
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
-if GPIO.getmode() == GPIO.BCM:
-    print('BCM pin layout already set')
-    pass
-else:
-    print('Setting pin layout mode')
-    GPIO.setmode(GPIO.BCM)
-
 #configure channels for blind control
-UP_GPIO_PIN = 2
-STOP_GPIO_PIN = 3
-DOWN_GPIO_PIN = 4
-chan_list = [UP_GPIO_PIN,STOP_GPIO_PIN,DOWN_GPIO_PIN]
-GPIO.setup(chan_list,GPIO.OUT)
+def initPins():
+    UP_GPIO_PIN = 2
+    STOP_GPIO_PIN = 3
+    DOWN_GPIO_PIN = 4
+    chan_list = [UP_GPIO_PIN,STOP_GPIO_PIN,DOWN_GPIO_PIN]
+    GPIO.setup(chan_list,GPIO.OUT)
 
-#define functions to control blinds via remote
-def pressButton(pin):           #analogous to pressing up button
-    GPIO.output(pin,GPIO.HIGH)  #power the pin
-    GPIO.output(pin,GPIO.LOW)   #remove the power
+#cleanup pins
+def resetPins():
+    GPIO.cleanup()
+    if GPIO.getmode() == GPIO.BCM:
+        print('BCM pin layout already set')
+        pass
+    else:
+        print('Setting pin layout mode')
+        GPIO.setmode(GPIO.BCM)
+
+#define button presses
+def pressButton(pin):                        #analogous to pressing up button
+    GPIO.output(pin, (GPIO.HIGH, GPIO.LOW))  #power the pin then remove the power
 
 def timedBlindMotion(pin, duration): #press directional button, then wait and press stop
     pressButton(pin)
     time.sleep(duration)
     pressButton(STOP_GPIO_PIN)
 
+resetPins()
 #define API routes
 @app.route("/hello")
 def hello():
