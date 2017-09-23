95bee604c96523be8654d7a31858a6ebc389f2c1
Paul Henry <phenry@Phenry-MBP.hsd1.ca.comcast.net>
2017-09-23 11:31:26 -0700

    comment out api routes, but have working functions to control blinds

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 #load API frameworks
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
 #load GPIO framework and set correct pin layout
 try:
@@ -8,38 +8,37 @@ try:
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
-UP_GPIO_PIN = 2
-STOP_GPIO_PIN = 3
-DOWN_GPIO_PIN = 4
+from time import sleep
+
+UP_GPIO_PIN = 3
+STOP_GPIO_PIN = 5
+DOWN_GPIO_PIN = 7
 
 #configure channels for blind control
-def initPins():
-    chan_list = [UP_GPIO_PIN,STOP_GPIO_PIN,DOWN_GPIO_PIN]
-    GPIO.setup(chan_list,GPIO.OUT)
+#def initPins():
+    #turns out when you initialize the
+    #GPIO pin (high or low) it "presses" the button
 
 #cleanup pins
 def resetPins():
     GPIO.cleanup()
-    if GPIO.getmode() == GPIO.BCM:
-        print('BCM pin layout already set')
-        pass
-    else:
-        print('Setting pin layout mode')
-        GPIO.setmode(GPIO.BCM)
+
 
 #define button presses
-def pressButton(pin):                        #analogous to pressing up button
-    GPIO.output(pin, (GPIO.HIGH, GPIO.LOW))  #power the pin then remove the power
+def pressButton(pin):
+    if GPIO.getmode() != GPIO.BOARD:   #set the board mode at time of press if not set
+        GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.OUT, initial=0)  #power the pin then remove the power
+    sleep(0.5)
+    GPIO.cleanup(pin)
 
 def timedBlindMotion(pin, duration): #press directional button, then wait and press stop
     pressButton(pin)
     time.sleep(duration)
     pressButton(STOP_GPIO_PIN)
 
-resetPins()
 #define API routes
-@app.route("/blinds", methods=['POST'])
-def blindControl():
-
+# @app.route("/blinds", methods=['POST'])
+# def blindControl():
     # client_id = request.args.get('client_id', '')
     # client_secret = request.args.get('client_secret', '')
