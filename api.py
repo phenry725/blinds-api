396b49bbf46bbf03d317007cb5f4f0d72f82cfd0
Paul Henry <paulh725@gmail.com>
2017-10-05 23:08:34 -0700

    extract the payload filtering to reuse for lights add significant commenting

--- api.py
+++ api.py
@@ -4,16 +4,21 @@ import RPi.GPIO as GPIO
 from flask import Flask, request, json
 app = Flask(__name__)
 
+#Pin numbers based on Board mapping for Raspbery Pi 3 Model B
 UP_GPIO_PIN = 37
 STOP_GPIO_PIN = 35
 DOWN_GPIO_PIN = 33
 
+#multi-step because it doesn't look like python supports simultaneous instansiation and load
 pinsDict = {}
 pinsDict['up'] = UP_GPIO_PIN
 pinsDict['down'] = DOWN_GPIO_PIN
 pinsDict['stop'] = STOP_GPIO_PIN
 
-#configure channels for blind control
+##############
+#GPIO CONTROL#
+##############
+#configure board for correct pin mapping
 def initPins():
     if GPIO.getmode() != GPIO.BOARD:   #set the board mode at time of press if not set
         GPIO.setmode(GPIO.BOARD)
@@ -22,17 +27,28 @@ def initPins():
 def resetPins():
     GPIO.cleanup()
 
-#define button presses
+#on remote, single press will cause steppers to progress to the limit in that direction
 def pressButton(pin):
     if GPIO.getmode() !=  GPIO.BOARD:
         initPins()
-    GPIO.setup(pin, GPIO.OUT, initial=0)  #power the pin then remove the power
-    sleep(0.5)
+    GPIO.setup(pin, GPIO.OUT, initial=0)  #setting up the pin causes the button to "press" on the remote
+    sleep(0.5)                            #add delay so the motors have time to get the command
     GPIO.cleanup(pin)
 
-def stopBlindsAfterDelay(duration): #press directional button, then wait and press stop
+#add a delay then stop, used for timed motion i.e. "go up for 3 seconds"
+def stopBlindsAfterDelay(duration):
     sleep(duration)
     pressButton(STOP_GPIO_PIN)
+
+#############
+##API UTILS##
+#############
+
+def filterEmptyPayload(httpRequest):
+    content = httpRequest.get_json()
+    if content is None:
+        return returnErrorStatus()
+    return content
 
 def returnErrorStatus():
     return json.jsonify(
@@ -46,24 +62,20 @@ def returnSuccessStatus():
 
 #define API routes
 @app.route("/blinds", methods=['POST'])
+#TODO:what if duration is not int, explain difference between press/timed
 def blindControl():
-    content = request.get_json()
-    if content is None:
-        return returnErrorStatus()
+    content = filterEmptyPayload(request)
     print "Incoming request: " + str(content)
-    #Try to set the pin for the command, if not return error
-    try
+    try                                             #Try to set the pin for the command, if not return error
         pin = pinsDict[content['command']]
     except KeyError:
         print "Error invalid or missing command: " + str(content['command'])
         return returnErrorStatus()
-    #execute pin press
     pressButton(pin)
     if 'duration' in content:
-        stopBlindsAfterDelay(content['duration']) #what if duration is not int, explain difference between press/timed
+        stopBlindsAfterDelay(content['duration'])
     return returnSuccessStatus()
 
 # @app.route("/lights", methods=['POST'])
 # def lightControl():
-#     content = request.get_json()
-#
+#     content = filterEmptyPayload(request)
